c678f9ca5515a717d51eee2c4202634e8ea1708f
Hnitrogen Li <1791422575@qq.com>
2022-09-19 13:54:45 +0800

    add compress.js

---
+++ scripts/compress_game_js.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# 这是一个打包的shell脚本
+# 他将 src里面的js代码打包放到dist里面去
+
+JS_PATH=/home/pink/acapp/game/static/js/    # 声明宏
+JS_PATH_DIST=${JS_PATH}dist/
+JS_PATH_SRC=${JS_PATH}src/ 
+
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+# 在src下找到js文件,并按照字典序输出到dist的game.js文件
