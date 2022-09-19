42377ed2dcb19ba138d3aa7e0cc65b701f6467ec
Hnitrogen Li <1791422575@qq.com>
2022-09-19 21:12:50 +0800

    1:25 | bug

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -1,5 +1,20 @@
-class AcGame {
+class AcGameMenu 
+{
+    constructor(root) 
+    { 
+        this.root = root ;  //存调用函数的对象 
+        this.$menu = $(`
+            <div class="ac-game-menu">
+            </div>
+        `);
+        
+        this.root.$ac_game.append(this.$menu);
+    }
+}class AcGame {
 	constructor(id) {
-		console.log("create AcGame");
+		//console.log("create AcGame");
+		this.id = id ;
+		this.$ac_game = $('#' + id);	//找到id为传入id 的 <div> ，用 ac_game 存起来 
+		this.menu = new AcGameMenu(this);
 	}
 }
--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -3,13 +3,11 @@ class AcGameMenu
     constructor(root) 
     { 
         this.root = root ;  //存调用函数的对象 
-        
-        this.$menu = $(
+        this.$menu = $(`
             <div class="ac-game-menu">
             </div>
-        );
+        `);
         
         this.root.$ac_game.append(this.$menu);
-
     }
 }
