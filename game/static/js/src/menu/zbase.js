bbfd546fa75b4f57fa1d7094972e19c65f4a34c8
Hnitrogen Li <1791422575@qq.com>
2022-09-20 16:07:50 +0800

    add menu-op

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -1,13 +1,32 @@
 class AcGameMenu 
 {
     constructor(root) 
-    { 
+    {  
         this.root = root ;  //存调用函数的对象 
         this.$menu = $(`
             <div class="ac-game-menu">
+                <div class="ac-game-menu-field">
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+                        单人模式
+                    </div>
+                    
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-muti">
+                        多人模式
+                    </div>
+                    
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+                        设置
+                    </div>
+                </div>
             </div>
         `);
         
         this.root.$ac_game.append(this.$menu);
+
+        //class 加. id是加 #
+        this.$single = this.$menu.find('.ac-game-menu-item-single');    //用find函数找到对应class 对应的 div
+        this.$muti = this.$menu.find('.ac-game-menu-item-field-muti');
+        this.$settings = this.$menu.find('.ac-game-menu-item-field-settings');
+
     }
 }
