af5f7967a53ada70c47daf1477511fc23df2a95b
Hnitrogen Li <1791422575@qq.com>
2022-09-21 00:52:54 +0800

    1:56 slp

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -6,11 +6,11 @@ class AcGameMenu
         this.$menu = $(`
             <div class="ac-game-menu">
                 <div class="ac-game-menu-field">
-                    <div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
                         单人模式
                     </div>
                     <br>
-                    <div class="ac-game-menu-field-item ac-game-menu-field-item-muti">
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-muti-mode">
                         多人模式
                     </div>
                     <br>
@@ -24,10 +24,22 @@ class AcGameMenu
         this.root.$ac_game.append(this.$menu);
 
         //class 加. id是加 #
-        this.$single = this.$menu.find('.ac-game-menu-item-single');    //用find函数找到对应class 对应的 div
-        this.$muti = this.$menu.find('.ac-game-menu-item-field-muti');
+        this.$single_mode = this.$menu.find('.ac-game-menu-item-single-mode');    //用find函数找到对应class 对应的 div
+        this.$muti_mode = this.$menu.find('.ac-game-menu-item-field-muti-mode');
         this.$settings = this.$menu.find('.ac-game-menu-item-field-settings');
+
+        this.start();   //启动新窗口
     }
 
-    
+    start() { 
+        this.add_listening_events();
+    }
+
+    add_listening_events()  {
+        let outer = this;
+        this.$single_mode.click(function()
+        {
+            console.log("click single_mode");
+        });
+    }
 }
