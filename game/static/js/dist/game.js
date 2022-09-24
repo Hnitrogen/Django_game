91c2ed247be8a0241573e10048796031cf051bd4
Hnitrogen <1791422575@qq.com>
2022-09-24 11:04:29 +0800

    canvas

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -64,13 +64,84 @@ class AcGameMenu
     hide()  {   //关闭menu界面
         this.$menu.hide();
     }
+}let AC_GAME_OBJECTS = [];
+
+class AcGameObject  {
+    constructor() {
+        AC_GAME_OBJECTS.push(this);
+
+        this.has_called_start = false;
+        this.timedelta = 0 ;
+    }
+
+    start() {
+    }
+
+    update() {
+    }
+    
+    on_destroy() {
+    }
+
+    destroy() {
+        this.on_destroy();
+
+        for(let i = 0 ; i < AC_GAME_OBJECTS.length ; i++)
+            if(AC_GAME_OBJECTS[i] === this) {
+                AC_GAME_OBJECTS.splice(i,1);
+                break;
+            }
+    }
+
+}
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp)  {
+    
+    for(let i = 0 ; i < AC_GAME_OBJECTS.length ; i++)   {
+        let obj = AC_GAME_OBJECTS[i];
+        if(!obj.has_called_start)   {
+            obj.start();
+            obj.has_called_start = true;
+        }   else {
+            obj.timedelta = timestamp - last_timestamp ;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp ;
+    
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION);class GameMap extends  AcGameObject  {
+    constructor(playground)  {
+        super();    // 调用基类构造函数
+        this.playground = playground;
+        this.$canvas = $(`<canvas></canvas>`);
+
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.playground.$playground.append(this.$canvas);
+
+    }
+
+    start()  {
+    }
+
+    update() {
+    }
+
+    
 }class AcGamePlayground  {
     constructor(root)   {
         this.root = root;
         this.$playground = $(`<div class="ac-game-playground">游戏界面</div>`)
         //this.hide();
         this.root.$ac_game.append(this.$playground);    //web.html里面的ac_game 
-        
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
         this.start();
     }
 
--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -4,7 +4,9 @@ class AcGamePlayground  {
         this.$playground = $(`<div class="ac-game-playground">游戏界面</div>`)
         //this.hide();
         this.root.$ac_game.append(this.$playground);    //web.html里面的ac_game 
-        
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
         this.start();
     }
 
