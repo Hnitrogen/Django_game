783e88ecc1ae31e2402dcd6cc9f6de82f0da3d60
Hnitrogen <1791422575@qq.com>
2022-12-05 14:34:59 +0800

    console status

--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -19,16 +19,17 @@ class Settings	{
 	
 	getinfo() {		// 获取服务器信息
 		let outer = this ; 
-		
-		$.ajax({
+
+		$.ajax({		// ajax是向url发请求 --- getinfo向后端发请求 --- 获取平台
 			url: "https://app3500.acapp.acwing.com.cn/settings/getinfo/",	 // 传入一个字典
 			type: "GET",
 			data: {
 				platform: outer.platform ,
 			},
 			success: function(resp)	{	// 回调函数
+				console.log(resp);
 				if(resp.result === "success")	{
-					outer.hide() ; 
+ 					outer.hide() ; 
 					outer.root.menu.show() ; 
 				}	else {
 					outer.login() ; 
