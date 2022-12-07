ed3b02d83527997039cfd5f90777d661eead53a6
Hnitrogen <1791422575@qq.com>
2022-12-07 23:55:01 +0800

    finish login-in-page

--- game/urls/settings/index.py
+++ game/urls/settings/index.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from game.views.settings.getinfo import getinfo         # 调包hhh
+from game.views.settings.login import signin
+from game.views.settings.logout import signout
+from game.views.settings.register import register
 
 urlpatterns = [
     path("getinfo/",getinfo,name="settings_getinfo") , 
+    path("login/",signin,name="settings_login") , 
+    path("logout/",signout,name="settings_logout"),
+    path("register/",register,name="setttings_register"),
 
     # game.urls/getinfo
 ]
--- static/js/src/menu/zbase.js
+++ static/js/src/menu/zbase.js
@@ -16,7 +16,7 @@ class AcGameMenu
                     </div>
                     <br>
                     <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
-                        设置
+                        退出
                     </div>
                 </div>
             </div>
@@ -55,6 +55,7 @@ class AcGameMenu
         this.$settings.click(function()
         {
             console.log("click settings_mode");
+            outer.root.settings.logout_on_remote() ; 
         });
     }
 
